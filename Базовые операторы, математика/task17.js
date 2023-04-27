8e7c9d4849159db0eaa563f3271d8ad7000bafaf
Hrumstik <gurinovich.warsaw@gmail.com>
2023-04-27 17:04:39 +0200

    removed files

--- Базовые операторы, математика/task17.js
+++
@@ -1,6 +0,0 @@
-// 17. Пользователь вводит сторону квадрата. Найдите периметр и площадь квадрата.
-let side = prompt("write the value of side", 0);
-let perimeter = side * 4;
-let area = side * side;
-alert("the perimeter of square is " + perimeter);
-alert("the area of square is " + area);
--- Базовые операторы, математика/task18.js
+++
@@ -1,7 +0,0 @@
-// 18. Пользователь вводит цены 1 кг конфет и 1 кг печенья. Найдите стоимость: а) одной покупки из 300 г конфет и 400 г печенья; б) трех покупок, каждая из 2 кг печенья и 1 кг 800 г конфет.
-let candy_price = prompt("write the price of candies for 1 kilo", 0);
-let cookie_price = prompt("write the price of cookie for 1 kilo", 0);
-let first_purchase = (candy_price * 0.3) + (cookie_price * 0.4);
-alert("first purchase cost is " + first_purchase);
-let second_purchase = ((candy_price * 2) + (cookie_price * 1.8)) * 3;
-alert("second purchase cost is " + second_purchase);

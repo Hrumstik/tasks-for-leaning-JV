bd69bf5abbb28b9b09e94707c60b32f988d933a2
Hrumstik <gurinovich.warsaw@gmail.com>
2023-04-27 17:14:07 +0200

    removed files from main branch

--- Циклы и условия/task15.js
+++
@@ -1,9 +0,0 @@
-// Выведите в консоль count элементов арифметической прогрессии с началом в start и шагом step.
-
-let start = +prompt("Введите стартовое число", 1);
-let step = +prompt("Введите число-шаг", 2);
-
-for (let count = start; start <= 1000; start++) {
-    count = count + step;
-    console.log(count);
-}
